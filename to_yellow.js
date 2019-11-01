0aba5d4c3078d48a8b96ca02892d062605ffd130
aweisenfeld3 <aewsong@Aryans-MacBook-Pro.local>
2019-11-01 19:39:03 -0400

    black to yellow

---
+++ to_yellow.js
@@ -0,0 +1,9 @@
+var img = new SimpleImage (200, 200);
+print(img);
+for (var pixel in img.values()) {
+    var newR = 255 - pixel.getRed();
+    var newG = 255 - pixel.getGreen();
+    pixel.setRed(newR);
+    pixel.setGreen(newG);
+}
+print(img)
