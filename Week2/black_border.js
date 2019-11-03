ae475323622f8ae03058fa252947e38795220ff8
aweisenfeld3 <aewsong@Aryans-MacBook-Pro.local>
2019-11-02 21:41:01 -0400

    black border

---
+++ Week2/black_border.js
@@ -0,0 +1,15 @@
+var img = new SimpleImage ("smallpanda.png"); 
+print(img)
+for (var pxl of img.values()) {
+    if (pxl.getX() < 10 || pxl.getX() > img.getWidth() - 10) {
+        pxl.setGreen(0);
+        pxl.setRed(0);
+        pxl.setBlue(0);
+    }
+    if (pxl.getY() < 10 || pxl.getY() > img.getHeight()-10) {
+        pxl.setGreen(0);
+        pxl.setRed(0);
+        pxl.setBlue(0);    
+    }
+}
+print(img);
