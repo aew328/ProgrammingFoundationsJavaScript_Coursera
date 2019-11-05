53b41c27177d9622a6f93f0d152248a26f5426ba
aweisenfeld3 <aewsong@Aryans-MacBook-Pro.local>
2019-11-04 20:35:51 -0500

    hmm

--- Week2/black_border.js
+++ Week2/black_border.js
@@ -9,7 +9,7 @@ for (var pxl of img.values()) {
     if (pxl.getY() < 10 || pxl.getY() > img.getHeight()-10) {
         pxl.setGreen(0);
         pxl.setRed(0);
-        pxl.setBlue(0);    
+        pxl.setBlue(255);    
     }
 }
 print(img);
